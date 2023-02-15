3d6f4fe2af3b5c65b74da1585167784e4ccb398e
Jared Flores <jaredfm24@gmail.com>
2023-02-15 16:17:35 -0600

    Segundo commit, se agrego hello

---
+++ app.js
@@ -0,0 +1,6 @@
+let numberOne = 1;
+let numberTwo = 3;
+
+const PLUS = numberOne + numberTwo;
+
+console.log(PLUS);
---
+++ app2.js
@@ -0,0 +1,6 @@
+let numberOne = 4;
+let numberTwo = 1;
+
+const ANS = numberOne - numberTwo;
+
+console.log(ANS);
